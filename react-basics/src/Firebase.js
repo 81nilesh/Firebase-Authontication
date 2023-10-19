09461e0c67d80a63a67518de2eb4ea5bb60778de
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-10-19 16:48:34 +0530

    first commit

---
+++ react-basics/src/Firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth;'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBXiQnszv47QULV2HpF3JW6oy-ETybgyZs",
+    authDomain: "fir-954de.firebaseapp.com",
+    projectId: "fir-954de",
+    storageBucket: "fir-954de.appspot.com",
+    messagingSenderId: "488915909430",
+    appId: "1:488915909430:web:5860d9fb66e31ddc775197"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
